699d8f772ecc3c506766d1ef59b9b4129c76d7a4
Juwan Kim <hellojuwan@gmail.com>
2019-02-12 17:16:19 -0600

    homework 2

---
+++ src/excersise3.py
@@ -0,0 +1,6 @@
+englishs = input('Enter English Score: ')
+maths= input('Enter Math Score: ')
+total_score = int(englishs) + int (maths)
+print ('Total Score:' ,total_score)
+Average= total_score/2
+print ('average score:' ,Average)
